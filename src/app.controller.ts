63b8bfb8151e755a8148af0acce700658d3c0ca1
Benjamin Muriel <benngiemurielito@gmail.com>
2023-08-02 22:17:04 -0400

    importando libreria

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Req } from '@nestjs/common';
+import { Body, Controller, Get,Delete, Param, Post, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Request } from 'express';
 
